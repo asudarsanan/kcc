f2911bccc92f6069633086e08ac829ff3e3bba8e
Aasish Sudarsanan <asudarsanan@protonmail.com>
2024-05-21 22:27:32 +0530

    First commit with initial working code for kcc :) - asuda!

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/asudarsanan/kcc
+
+go 1.20
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/tview v0.0.0-20240519200218-0ac5f73025a8 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/manifoldco/promptui"
+	"gopkg.in/yaml.v2"
+)
+
+type KubeConfig struct {
+	APIVersion     string                 `yaml:"apiVersion"`
+	Clusters       []Cluster              `yaml:"clusters"`
+	Contexts       []Context              `yaml:"contexts"`
+	CurrentContext string                 `yaml:"current-context"`
+	Kind           string                 `yaml:"kind"`
+	Preferences    map[string]interface{} `yaml:"preferences"`
+	Users          []User                 `yaml:"users"`
+}
+
+type Cluster struct {
+	Name    string                 `yaml:"name"`
+	Cluster map[string]interface{} `yaml:"cluster"`
+}
+
+type Context struct {
+	Name    string                 `yaml:"name"`
+	Context map[string]interface{} `yaml:"context"`
+}
+
+type User struct {
+	Name string                 `yaml:"name"`
+	User map[string]interface{} `yaml:"user"`
+}
+
+func readKubeConfig(filePath string) (*KubeConfig, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config KubeConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func writeKubeConfig(filePath string, config *KubeConfig) error {
+	data, err := yaml.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filePath, data, 0644)
+}
+
+func switchContext(config *KubeConfig, contextName string) error {
+	for _, context := range config.Contexts {
+		if context.Name == contextName {
+			config.CurrentContext = contextName
+			return nil
+		}
+	}
+	return fmt.Errorf("context %s not found", contextName)
+}
+
+func showSelector(options []string) (string, error) {
+	prompt := promptui.Select{
+		Label: "Select Kubernetes cluster context:",
+		Items: options,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func main() {
+	kubeConfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+
+	config, err := readKubeConfig(kubeConfigPath)
+	if err != nil {
+		fmt.Printf("Error reading kubeconfig: %v\n", err)
+		return
+	}
+
+	contexts := make([]string, len(config.Contexts))
+	for i, ctx := range config.Contexts {
+		contexts[i] = ctx.Name
+	}
+
+	selectedContext, err := showSelector(contexts)
+	if err != nil {
+		fmt.Printf("Error selecting context: %v\n", err)
+		return
+	}
+
+	err = switchContext(config, selectedContext)
+	if err != nil {
+		fmt.Printf("Error switching context: %v\n", err)
+		return
+	}
+
+	err = writeKubeConfig(kubeConfigPath, config)
+	if err != nil {
+		fmt.Printf("Error writing kubeconfig: %v\n", err)
+	}
+
+	fmt.Printf("Switched to context %s\n", selectedContext)
+}
